1100595e7ae1cfc33d63f69f54f616231e7b2e16
Ilya Holuban <ilyholu@gmail.com>
2024-12-08 17:46:17 +0200

    tailwind font, colors configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-
 export default function App() {
-  return <Header />;
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <div className="font-semibold text-3xl">Hello world! Привет, мир!</div>
+      <div className="w-40 h-40 bg-accent mb-10"></div>
+      <div className="w-40 h-40 bg-primaryLight mb-10"></div>
+      <div className="w-40 h-40 bg-secondary mb-10"></div>
+      <div className="w-40 h-40 bg-secondaryNeutral mb-10"></div>
+      <div className="w-40 h-40 bg-neutral-800 mb-10"></div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryLight: "#F1F1F1",
+        secondary: "#FF8C00",
+        secondaryNeutral: "#D9D9D9",
+        accent: "#FF4500",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
