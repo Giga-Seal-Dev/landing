8d6887a4c8eabe96d6692898b6d00fbeeace2590
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2024-12-12 13:49:50 +0200

    Reapply "app component fix"
    This reverts commit d447c25aa86cc47360979bc8d7758c9b4ba8a718.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import MainSection from "./sections/MainSection";
 import Header from './components/Header';
+import MainSection from './sections/MainSection';
 
 export default function App() {
-  return 
-<>
-  <Header />
-  <MainSection />
-</>
-;}
+	return (
+		<>
+			<Header />
+			<MainSection />
+		</>
+	);
+}
