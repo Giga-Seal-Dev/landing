45c1a2a78be782d6916b85b234cbd32c2493a7b9
Ilya Holuban <ilyholu@gmail.com>
2024-12-15 20:50:46 +0200

    solutions section cards markup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,20 @@
 type PropsType = {
-	children: string;
-	type: string;
-	link: string;
+  children: string;
+  type: string;
+  link?: string;
 };
 
 export default function Button({ children, type, link }: PropsType) {
-	return (
-		<button
-			className={`self-center md:self-start border-2 border-accent ${
-				type === 'colorful' ? 'bg-accent text-primaryLight' : ''
-			}
+  return (
+    <button
+      className={`self-center md:self-start border-2 border-accent ${
+        type === "colorful" ? "bg-accent text-primaryLight" : ""
+      }
 			${
-				type === 'transparent' ? 'bg-primaryLight text-accent' : ''
-			} rounded-xl px-6 leading-10`}
-		>
-			{children}
-		</button>
-	);
+        type === "transparent" ? "bg-primaryLight text-accent" : ""
+      } rounded-xl px-6 leading-10`}
+    >
+      {children}
+    </button>
+  );
 }
