8841bdd9c9e7c1242449f1f878966e389b8541d3
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2025-01-14 10:42:42 +0200

    changed features section layout and bg

--- src/sections/FeaturesSection.tsx
+++ src/sections/FeaturesSection.tsx
@@ -1,95 +1,114 @@
-import IntegrationsTile from "../components/IntegrationsTile";
+import IntegrationsTile from '../components/IntegrationsTile';
 
 import { motion } from 'motion/react';
 type IntegraionsType = {
-  name: string;
-  iconPath: string;
+	name: string;
+	iconPath: string;
 };
 
 export default function FeaturesSection() {
-  const integragionsList: IntegraionsType[] = [
-    { name: "Zoom", iconPath: "/assets/integrations_icons/zoom.png" },
-    {
-      name: "Google Calendar",
-      iconPath: "/assets/integrations_icons/google_calendar.png",
-    },
-    {
-      name: "Google Meet",
-      iconPath: "/assets/integrations_icons/google_meet.png",
-    },
-    {
-      name: "AI Assistant",
-      iconPath: "/assets/integrations_icons/ai.png",
-    },
-    { name: "Telegram", iconPath: "/assets/integrations_icons/telegram.png" },
-    { name: "Viber", iconPath: "/assets/integrations_icons/viber.png" },
-    {
-      name: "Stripe",
-      iconPath: "/assets/integrations_icons/stripe.png",
-    },
-    { name: "Monobank", iconPath: "/assets/integrations_icons/monobank.png" },
-  ];
+	const integragionsList: IntegraionsType[] = [
+		{ name: 'Zoom', iconPath: '/assets/integrations_icons/zoom.png' },
+		{
+			name: 'Google Calendar',
+			iconPath: '/assets/integrations_icons/google_calendar.png',
+		},
+		{
+			name: 'Google Meet',
+			iconPath: '/assets/integrations_icons/google_meet.png',
+		},
+		{
+			name: 'AI Assistant',
+			iconPath: '/assets/integrations_icons/ai.png',
+		},
+		{ name: 'Telegram', iconPath: '/assets/integrations_icons/telegram.png' },
+		{ name: 'Viber', iconPath: '/assets/integrations_icons/viber.png' },
+		{
+			name: 'Stripe',
+			iconPath: '/assets/integrations_icons/stripe.png',
+		},
+		{ name: 'Monobank', iconPath: '/assets/integrations_icons/monobank.png' },
+	];
 
 	return (
 		<section
 			id='features'
-			className='h-screen flex flex-wrap items-center content-center py-8 px-12 bg-primaryLight text-neutral-800 text-center gap-y-5'
+			className='min-h-screen flex flex-col gap-y-4 items-center py-10 content-center text-neutral-800 scroll-my-20'
 		>
-			<div className='w-full'>
-				<h2 className='text-4xl mb-7 font-semibold'>
+			<div className='container'>
+				<h2 className='text-4xl mb-7 font-semibold text-center'>
 					Чому треба обрати саме нашу платформу?
 				</h2>
-			</div>
-			<div className='flex flex-col w-1/2 gap-y-3'>
-				<motion.div
-					initial={{ opacity: 0, x: -50 }}
-					whileInView={{ opacity: 1, x: 0 }}
-					transition={{ duration: 0.6 }}
-					className='p-4 border shadow-lg w-full rounded-xl border-l-8 border-l-accent'
-				>
-					<h3 className='text-xl font-bold'>Геймифікація</h3>
-					<p className='mt-1'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
-						atque laborum dolorem numquam dignissimos, laudantium distinctio
-						error tempore nisi minus nemo vero quos, accusamus quae amet
-						voluptatibus alias asperiores. Laboriosam dolore perspiciatis
-						inventore illum provident nulla aliquam quis? Maxime, eveniet!
-					</p>
-				</motion.div>
+				<div className='container flex justify-center gap-x-6'>
+					<div className='w-1/2 flex flex-col items-end gap-y-3'>
+						<motion.div
+							initial={{ opacity: 0, x: -50 }}
+							whileInView={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.6 }}
+							className='flex flex-col gap-y-4 p-8 border shadow-lg w-5/6 rounded-xl border-l-8 border-l-accent'
+						>
+							<h3 className='text-xl font-bold text-center'>
+								Статистика та аналітика
+							</h3>
+							<p className='mt-1'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+								voluptate debitis dolore expedita non commodi quia saepe
+								delectus cupiditate nostrum?
+							</p>
+						</motion.div>
 
-				<motion.div
-					initial={{ opacity: 0, x: -50 }}
-					whileInView={{ opacity: 1, x: 0 }}
-					transition={{ duration: 0.6 }}
-					className='p-4 border shadow-lg w-full rounded-xl border-l-8 border-l-accent'
-				>
-					<h3 className='text-xl font-bold'>Аналітика</h3>
-					<p className='mt-1'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
-						atque laborum dolorem numquam dignissimos, laudantium distinctio
-						error tempore nisi minus nemo vero quos, accusamus quae amet
-						voluptatibus alias asperiores. Laboriosam dolore perspiciatis
-						inventore illum provident nulla aliquam quis? Maxime, eveniet!
-					</p>
-				</motion.div>
-				<motion.div
-					initial={{ opacity: 0, x: -50 }}
-					whileInView={{ opacity: 1, x: 0 }}
-					transition={{ duration: 0.6 }}
-					className='p-4 border shadow-lg w-full rounded-xl border-l-8 border-l-accent'
-				>
-					<h3 className='text-xl font-bold'>Кросплатформеність</h3>
-					<p className='mt-1'>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
-						atque laborum dolorem numquam dignissimos, laudantium distinctio
-						error tempore nisi minus nemo vero quos, accusamus quae amet
-						voluptatibus alias asperiores. Laboriosam dolore perspiciatis
-						inventore illum provident nulla aliquam quis? Maxime, eveniet!
-					</p>
-				</motion.div>
+						<motion.div
+							initial={{ opacity: 0, x: -50 }}
+							whileInView={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.6 }}
+							className='flex flex-col gap-y-4 p-8 border shadow-lg w-5/6 rounded-xl border-l-8 border-l-accent'
+						>
+							<h3 className='text-xl font-bold text-center'>
+								Кросплатформерність
+							</h3>
+							<p className='mt-1'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+								voluptate debitis dolore expedita non commodi quia saepe
+								delectus cupiditate nostrum?
+							</p>
+						</motion.div>
+						<motion.div
+							initial={{ opacity: 0, x: -50 }}
+							whileInView={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.6 }}
+							className='flex flex-col gap-y-4 p-8 border shadow-lg w-5/6 rounded-xl border-l-8 border-l-accent'
+						>
+							<h3 className='text-xl font-bold text-center'>
+								Створення сертифікатів
+							</h3>
+							<p className='mt-1'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+								voluptate debitis dolore expedita non commodi quia saepe
+								delectus cupiditate nostrum?
+							</p>
+						</motion.div>
+					</div>
+					<div className='w-1/2 h-auto'>
+						<motion.div
+							initial={{ opacity: 0, x: -50 }}
+							whileInView={{ opacity: 1, x: 0 }}
+							transition={{ duration: 0.6 }}
+							className='flex flex-col gap-y-4 p-8 border shadow-lg w-5/6 h-full rounded-xl border-l-8 border-l-accent justify-center'
+						>
+							<h3 className='text-xl font-bold text-center'>
+								Вбудований конструктор курсів
+							</h3>
+							<p className='mt-1'>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+								voluptate debitis dolore expedita non commodi quia saepe
+								delectus cupiditate nostrum?
+							</p>
+						</motion.div>
+					</div>
+				</div>
 			</div>
-			<div className='w-1/2 flex flex-col font-bold h-'>
-				<h3 className='text-xl'>Інтеграції</h3>
+			<div className='w-full flex flex-col font-bold py-8'>
+				<h3 className='text-xl text-center'>Інтеграції</h3>
 				<div className='flex flex-wrap mt-3 leading-relaxed relative gap-5 justify-center'>
 					{integragionsList.map((item, index) => (
 						<IntegrationsTile key={index} icon={item.iconPath}>
