49b56e34cf7df2bb21ddb1b34e5b1e235452a8cb
Ilya <ilyholu@gmail.com>
2025-01-14 13:20:26 +0200

    faq techsupport layout

--- src/sections/FaqSection.tsx
+++ src/sections/FaqSection.tsx
@@ -1,5 +1,7 @@
 import Dropdown from "../components/Dropdown";
 import faq from "../assets/faq.svg";
+import Button from "../components/Button";
+import { motion } from "motion/react";
 
 const dropHeadings = [
   "Що таке LMS і як вона працює?",
@@ -23,8 +25,9 @@ const dropTexts = [
 
 export default function FaqSection() {
   return (
-    <section id="faq" className="bg-primaryLight text-black py-10">
-      <div className="container mx-auto">
+    <section id="faq" className="bg-primaryLight text-black py-10 relative">
+      <div className="w-[400px] h-[400px] rounded-full z-10 opacity-10 bg-accent absolute -bottom-[100px]  -left-[100px]"></div>
+      <div className="container mx-auto flex flex-col space-y-4">
         <h2 className="text-4xl mb-7 font-semibold text-center">
           Часті запитання про нашу платформу
         </h2>
@@ -38,8 +41,26 @@ export default function FaqSection() {
 
           {/* SECOND COLUMN */}
           <div className="w-full hidden lg:flex lg:w-1/2 items-center justify-center">
-            <img src={faq} alt="faq image" className="max-w-[600px]" />
+            <img src={faq} alt="faq image" className="max-w-[500px]" />
           </div>
+        </div>
+        {/* Help */}
+        <div className="flex flex-col justify-center items-center space-y-2 ">
+          <h3 className="text-3xl mb-7 font-semibold text-center relative">
+            Не знайшли відповідь на своє питання?
+            <div className="w-16 h-16 rounded-full z-10 opacity-45 bg-accent blur-md absolute top-0 left-0"></div>
+            <div className="w-16 h-16 rounded-full z-10 opacity-45 bg-accent blur-xl absolute bottom-0 right-0"></div>
+          </h3>
+          <p className="text-lg text-center mb-2">
+            Звертайтеся до нашої служби підтримки, і ми з радістю вам
+            допоможемо!
+          </p>
+          <Button
+            children="Написати нам"
+            type="colorful"
+            size="medium"
+            isHeader={true}
+          />
         </div>
       </div>
     </section>
