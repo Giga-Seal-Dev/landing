a0a20253679de93d10296d96baee1bff16bc4a3a
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2025-01-14 11:40:58 +0200

    modified text color changing mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import Header from "./components/Header/Header";
-import FeaturesSection from "./sections/FeaturesSection";
-import Footer from "./sections/Footer";
-import MainSection from "./sections/MainSection";
-import Solutions from "./sections/Solutions";
+import Header from './components/Header/Header';
+import { HeaderTextColorProvider } from './components/Header/HeaderContext';
+import FeaturesSection from './sections/FeaturesSection';
+import Footer from './sections/Footer';
+import MainSection from './sections/MainSection';
+import Solutions from './sections/Solutions';
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <MainSection />
-      <FeaturesSection />
-      <Solutions />
-
-      <Footer />
-     
-    </>
-  );
+	return (
+		<>
+			<HeaderTextColorProvider>
+				<Header />
+				<MainSection />
+				<FeaturesSection />
+				<Solutions />
+				<Footer />
+			</HeaderTextColorProvider>
+		</>
+	);
 }
